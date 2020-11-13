3969f1469349909ff0df242542c7a1b131383f34
debargha-brainium <Deb!@#$5678>
2020-11-13 14:08:19 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { MainSidebarComponent } from './common/main-sidebar/main-sidebar.component';
+import { ControlSidebarComponent } from './common/control-sidebar/control-sidebar.component';
+import { HomeComponent } from './admin/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    LoginComponent,
+    FooterComponent,
+    MainSidebarComponent,
+    ControlSidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
